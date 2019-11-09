962c2c33c864ff7fc0fd54171f10288071a5cd26
Dylan <kite.d92@gmail.com>
2019-11-09 09:33:12 -0800

    edit knex configuration

--- db/knex.js
+++ db/knex.js
@@ -2,8 +2,10 @@ const credentials = require('../../credentials')
 const knex = require('knex')({
   client: 'mysql',
   version: '5.7',
+  debug: true,
   connection: {
     host: '10.132.118.121',
+    user: 'dylan',
     password: credentials.mysqlPassword,
     database: 'personal_site'
   }
