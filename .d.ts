95c20035af07e05ced0fa3fff94f22d7c8d4d06f
Kite <e48f@M-C02X73SVJHD2.nordstrom.net>
2019-12-21 17:44:09 -0800

    delete d.ts

--- .d.ts
+++
@@ -1,8 +0,0 @@
-declare module "redis" {
-
-    export interface RedisClient extends NodeJS.EventEmitter {
-        setAsync(key:string, value:string): Promise<void>;
-        getAsync(key:string): Promise<string>;
-    }
-
-}
