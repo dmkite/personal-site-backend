fd778729c23f43a22c8734336f9538e606bbdce8
Kite <e48f@M-C02X73SVJHD2.nordstrom.net>
2019-12-22 19:19:15 -0800

    create auth model and controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,13 +1,43 @@
 import authModel from '../models/auth'
+import bcrypt from 'bcrypt'
 
 interface IAuthController {
-  getPassword(): string
-  changePassword(oldPassword: string, newPassword:string): boolean
-  cryptify(password:string):string
+  changePassword(oldPassword: string, newPassword: string): Promise<boolean>
+  hashify(password: string): Promise<string>
+  passwordsMatch(password: string): Promise<boolean>
 }
 
 class AuthController implements IAuthController {
-  
+  saltRounds: number
+  constructor() {
+    this.saltRounds = 10
+  }
+
+  changePassword = async (password: string): Promise<boolean> => {
+    try {
+      if (this.passwordsMatch(password)) {
+        
+      }
+    } catch(err) {
+
+    }
+  }
+
+  hashify = async (password: string): Promise<string> => {
+    try {
+      const hashedPassword: string = await bcrypt.hash(password, this.saltRounds)
+      return hashedPassword
+    } catch (err) {
+      console.log(err)
+      return null
+    }
+  }
+
+  passwordsMatch = async(password:string): Promise<boolean> => {
+    const storedPassword: string = await authModel.getPassword()
+    const hashedPassword: string = await this.hashify(password)
+    return storedPassword === hashedPassword
+  }
 }
 
 const authController = new AuthController()
