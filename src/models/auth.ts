5942fddff6f34d05cfafb09c9fa123e2602f9e80
Kite <e48f@M-C02X73SVJHD2.nordstrom.net>
2019-12-26 09:22:19 -0800

    update model auth to correctly use super

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,35 +1,38 @@
-import { Model } from './Model'
-import { RedisKeys } from '../utils/redisConstants'
+import { RedisKeys } from "../utils/redisConstants";
+import { Model } from "./Model";
 
 interface IAuthModel {
-  getPassword(): Promise<string>
-  changePassword(password: string): Promise<boolean>
+  getPassword(): Promise<string>;
+  changePassword(password: string): Promise<boolean>;
 }
 
 class AuthModel extends Model implements IAuthModel {
+  constructor() {
+    super();
+  }
 
-  getPassword = async (): Promise<string> => {
-    const client = super.createClient()
+  public getPassword = async (): Promise<string> => {
+    const client = this.createClient();
     try {
-      const password = await client.getAsync(RedisKeys.Auth)
-      return password
+      const password = await client.getAsync(RedisKeys.Auth);
+      return password;
     } catch (err) {
-      console.log(err)
-      return null
+      console.log(err);
+      return null;
     }
   }
 
-  changePassword = async (newPassword: string): Promise<boolean> => {
-    const client = super.createClient()
+  public changePassword = async (newPassword: string): Promise<boolean> => {
+    const client = this.createClient();
     try {
-      await client.setAsync(RedisKeys.Auth, newPassword)
-      return true
+      await client.setAsync(RedisKeys.Auth, newPassword);
+      return true;
     } catch (err) {
-      console.log(err)
-      return false
+      console.log(err);
+      return false;
     }
   }
 }
 
-const authModel = new AuthModel()
-export default authModel
+const authModel = new AuthModel();
+export default authModel;
